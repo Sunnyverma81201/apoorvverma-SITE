bb930e484ea6ba3df6e1484c2744a8a6b3f070c2
Apoorv <sunny.verma81201@gmail.com>
2022-05-25 16:40:14 +0530

    test push

---
+++ resources/js/index.js
@@ -0,0 +1,8 @@
+
+window.onresize(()=>{
+        var width = screen.width;
+        var height = screen.height;
+        var hwDisplay = document.querySelector('#height-width');
+        hwDisplay.innerHTML = height + " x " + width;
+    }
+);
